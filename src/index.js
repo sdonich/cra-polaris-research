734ca65671abffe9f5023d7065aef54c0bb7ccb1
sdonich <sdonich@gmail.com>
2019-05-03 22:03:27 +0200

    nc

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import '@shopify/polaris/styles.css';
 import {AppProvider} from '@shopify/polaris';
 
-import App from './Frame';
+import App from './Page';
 
 ReactDOM.render(
   <AppProvider>
